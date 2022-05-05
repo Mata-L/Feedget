bdf95b9342625160a126af571397e55e12bb0f0c
Luan da Mata <ll.s2@hotmail.com>
2022-05-05 03:09:54 -0300

    Aula 3
    Conclusão do código da terceira aula do treinamento NLW

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b3eb814399bbe4",
+    pass: "76100b33a1c36a",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Luan da Mata <ll.s2@hotmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
